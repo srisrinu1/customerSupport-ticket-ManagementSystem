2fbb51d0dd918b24d19338bfb14bf3dd86581d1b
srisrinu1 <srinivasreddychalla133@gmail.com>
2023-08-06 23:25:52 +0530

    Accumulated the routes with multiple functions

--- middleware/validators/user.js
+++ middleware/validators/user.js
@@ -3,7 +3,7 @@ const joi=require('joi');
 const validateUser=async(req,res,next)=>{
 
 const userSchema=joi.object().strict().keys({
-   user:joi.string().min(8).max(30).required(),
+   username:joi.string().min(8).max(30).required(),
    email:joi.string().email().required(),
    password:joi.string().min(8).max(30).
    regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[a-zA-Z\d!@#$%^&*()_+]{8,30}$/).
@@ -17,7 +17,7 @@ try{
 }
 catch(error){
 //   console.log(error.details[0].message);
-  res.status(422).json({message:error.details[0].message});
+  res.status(400).json({message:error.details[0].message});
 }
 
 
