685f80ecb692322e6c6c80ffe0f0b5b4ab8eabac
srisrinu1 <srinivasreddychalla133@gmail.com>
2023-08-06 23:26:43 +0530

    Added Multiple functions to the routes

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,13 @@
 const express=require('express');
 const router=express.Router();
+const {registerUser,loginUser,currentUser}=require('../controllers/userController');
+const validateUser=require('../middleware/validators/user');
+const validateToken=require('../middleware/validateToken')
 
-router.post("/register",(req,res)=>{
-    res.json({message:"Register the user"});
-});
+router.post("/register",validateUser,registerUser);
 
-router.post("/login",(req,res)=>{
-    res.json({message:"login user"});
-});
+router.post("/login",loginUser);
 
-router.get("/current",(req,res)=>{
-    res.json({message:"Current user information"});
-});
+router.get("/current",validateToken,currentUser);
 
 module.exports=router;
